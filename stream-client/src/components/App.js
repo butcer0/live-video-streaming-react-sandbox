8e7961651942590946dae1f998a4b80564d8121a
Erik Butcher <butcer0@gmail.com>
2020-06-13 17:32:16 -0400

    Component Scaffolding with Routing
    Component Scaffolding with Routing

--- stream-client/src/components/App.js
+++ stream-client/src/components/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 
-const PageOne = () => {
-    return (
-        <div>
-            Page One
-            <br />
-            <Link to='/page-two'>Page Two</Link>
-        </div>
-    )
-
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            Page Two
-            <br />
-            <Link to=''>Page One</Link>
-        </div>
-    )
-}
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
-                <Route path="/" exact component={PageOne}/>
-                <Route path="/page-two" component={PageTwo}/>
+                <Route path="/" exact component={StreamList}/>
+                <Route path="/streams/new" component={StreamCreate}/>
+                <Route path="/streams/edit" component={StreamEdit}/>
+                <Route path="/streams/delete" component={StreamDelete}/>
+                <Route path="/streams/show" component={StreamShow}/>
             </div>
         </BrowserRouter>
     )
---
+++ stream-client/src/components/streams/index.js
@@ -0,0 +1,7 @@
+import StreamCreate from "./StreamCreate";
+import StreamDelete from "./StreamDelete";
+import StreamEdit from "./StreamEdit";
+import StreamList from "./StreamList";
+import StreamShow from "./StreamShow";
+
+export default { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow }
